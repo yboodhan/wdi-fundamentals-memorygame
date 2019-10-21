2ef7f2f5a1566bf27b3df6424d66d5c186a32663
Nalo <nalo@Ishans-MacBook-Pro.local>
2019-10-21 16:49:06 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,21 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
+function checkForMatch() {
+		if (cardsInPlay.length === 2) {
+			if (cardsInPlay[0] === cardsInPlay[1]) {
+	  			console.log("You found a match!");
+			} else {
+	  			console.log("Sorry, try again.");
+			}
 	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId] + ".");
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
